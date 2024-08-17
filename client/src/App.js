57f1cbca19b614f14f726cdb96ce9991ad982f4b
Amir Hamza <a.hamza8596@gmail.com>
2024-08-17 19:12:55 -0400

    Removed submodule configuration and re-added client folder

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ScraperForm from './components/scraperForm';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <ScraperForm />
+    </div>
+  );
+}
+
+export default App;
